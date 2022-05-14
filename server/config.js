67790bb4d32692d8f160d02939d56ac108095992
bumju <bj805225@gmail.com>
2022-05-14 15:56:52 +0900

    database setting

---
+++ server/config.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const config={
+    jwt:{
+        secretKey:process.env.JMT_SECRET,
+        expiresInSec:parseInt(process.env.JMT_EXPIRES),
+    },
+    bcrypt:{
+        saltRounds:parseInt(process.env.BCRYPT_SALT)
+    },
+    host: {
+      port: parseInt(process.env.HOST_PORT),
+    },
+    db:{
+        host:process.env.DB_HOST
+    }
+}
